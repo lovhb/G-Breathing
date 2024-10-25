bb4ee4781ec8a1a2d83ec9f20d6944fddbde6cae
lovhb <142816157+lovhb@users.noreply.github.com>
2024-10-25 19:35:16 +0200

    Optimized and cleaned up code

--- GBreathing/Main.cs
+++ GBreathing/Main.cs
@@ -14,52 +14,53 @@ namespace GBreathing;
 [ItemId("NotPolar.GBreathing")]
 public class Main : VtolMod
 {
-    public static string ModFolder;
-    private static AudioSource audioSource;
-    private static AudioSource endingAudioSource;
+    private static string _modFolder;
+    private static AudioSource _audioSource;
+    private static AudioSource _endingAudioSource;
     public AudioClip breathingClip;
     public AudioClip endingClip;
-    private static FlightInfo flightInfo;
+    private static FlightInfo _flightInfo;
     private const float VolumeReductionFactor = 0.5f;
     public AudioMixerGroup interiorMixerGroup;
 
     private void Awake()
     {
-        ModFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        Log($"Awake at {ModFolder}");
+        _modFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (_modFolder == null)
+        {
+            LogError("Mod folder path is null");
+            return;
+        }
+        Log($"Awake at {_modFolder}");
 
-        // Add AudioSource components
-        audioSource = gameObject.AddComponent<AudioSource>();
-        endingAudioSource = gameObject.AddComponent<AudioSource>();
+        // Add and configure AudioSource components
+        _audioSource = ConfigureAudioSource(gameObject.AddComponent<AudioSource>(), true);
+        _endingAudioSource = ConfigureAudioSource(gameObject.AddComponent<AudioSource>(), false);
         Log("AudioSource components added");
 
-        // Set the loop property to true
-        audioSource.loop = true;
-
         // Load the audio clips
-        var audioPath = Path.Combine(ModFolder, "GBreath.ogg");
-        var endingAudioPath = Path.Combine(ModFolder, "GBreath_End.ogg");
-        Log($"Loading audio from path: {audioPath}");
-        StartCoroutine(LoadAudio(audioPath, clip => breathingClip = clip, audioSource));
-        Log($"Loading ending audio from path: {endingAudioPath}");
-        StartCoroutine(LoadAudio(endingAudioPath, clip => endingClip = clip, endingAudioSource));
+        StartCoroutine(LoadAudio(Path.Combine(_modFolder, "GBreath.ogg"), clip => breathingClip = clip, _audioSource));
+        StartCoroutine(LoadAudio(Path.Combine(_modFolder, "GBreath_End.ogg"), clip => endingClip = clip, _endingAudioSource));
+    }
+    
+    private static AudioSource ConfigureAudioSource(AudioSource source, bool loop)
+    {
+        source.loop = loop;
+        return source;
     }
 
     public override void UnLoad()
     {
         Log("Unloading mod");
-        // Destroy any objects
-        Destroy(audioSource);
-        Destroy(endingAudioSource);
-
-        // Unload any resources
+        Destroy(_audioSource);
+        Destroy(_endingAudioSource);
         Resources.UnloadAsset(breathingClip);
         Resources.UnloadAsset(endingClip);
     }
 
     public static void SetFlightInfo(FlightInfo info)
     {
-        flightInfo = info;
+        _flightInfo = info;
         Log("FlightInfo component set");
     }
 
@@ -68,10 +69,9 @@ public class Main : VtolMod
         Log("Setting mixer group");
         if (group != null)
         {
-            // Set the mixer group
-            audioSource.outputAudioMixerGroup = group;
-            endingAudioSource.outputAudioMixerGroup = group;
-            Log("Mixer group set to" + group.name);
+            _audioSource.outputAudioMixerGroup = group;
+            _endingAudioSource.outputAudioMixerGroup = group;
+            Log("Mixer group set to " + group.name);
         }
         else
         {
@@ -82,93 +82,78 @@ public class Main : VtolMod
     private IEnumerator LoadAudio(string path, Action<AudioClip> setClip, AudioSource source)
     {
         Log($"Starting audio load coroutine for path: {path}");
-        using (var www = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.OGGVORBIS))
-        {
-            yield return www.SendWebRequest();
+        using var www = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.OGGVORBIS);
+        yield return www.SendWebRequest();
 
-            if (www.result == UnityWebRequest.Result.ConnectionError ||
-                www.result == UnityWebRequest.Result.ProtocolError)
-            {
-                LogError($"Error loading audio: {www.error}");
-            }
-            else
-            {
-                var clip = DownloadHandlerAudioClip.GetContent(www);
-                setClip(clip);
-                source.clip = clip;
-                Log("Audio clip loaded successfully");
-            }
+        if (www.result == UnityWebRequest.Result.ConnectionError ||
+            www.result == UnityWebRequest.Result.ProtocolError)
+        {
+            LogError($"Error loading audio: {www.error}");
+        }
+        else
+        {
+            var clip = DownloadHandlerAudioClip.GetContent(www);
+            setClip(clip);
+            source.clip = clip;
+            Log("Audio clip loaded successfully");
         }
     }
 
     private void Update()
     {
-        if (flightInfo != null)
+        if (_flightInfo == null) return;
+
+        //Log($"Current player Gs: {flightInfo.playerGs}");
+        var volume = CalculateVolume(_flightInfo.playerGs);
+        var flybyCamera = FlybyCameraMFDPage.instance;
+
+        if (_flightInfo.playerGs > 6)
+            PlayBreathing();
+        else
+            StopBreathing();
+
+        if (flybyCamera.cameraAudio && !flybyCamera.isInterior)
         {
-            //Log($"Current player Gs: {flightInfo.playerGs}");
-            var volume = CalculateVolume(flightInfo.playerGs);
-
-            if (flightInfo.playerGs > 6)
-                PlayBreathing();
-            else
-                StopBreathing();
-
-            if (FlybyCameraMFDPage.instance.cameraAudio && !FlybyCameraMFDPage.instance.isInterior)
-            {
-                audioSource.volume = 0;
-                endingAudioSource.volume = 0;
-                //Log("Muting audio because player is in exterior view");
-            }
-            else
-            {
-                audioSource.volume = volume * VolumeReductionFactor;
-                endingAudioSource.volume = VolumeReductionFactor;
-                //Log("Setting audio volume to " + volume);
-            }
+            _audioSource.volume = 0;
+            _endingAudioSource.volume = 0;
+        }
+        else
+        {
+            _audioSource.volume = volume * VolumeReductionFactor;
+            _endingAudioSource.volume = VolumeReductionFactor;
         }
     }
 
-    private float CalculateVolume(float playerGs)
+    private static float CalculateVolume(float playerGs)
     {
         // Assuming Gs range from 0 to 10, map this to volume range 0 to 1 using a quadratic function
         var normalizedGs = Mathf.Clamp(playerGs / 10f, 0f, 1f);
         return normalizedGs * normalizedGs; // Quadratic function for smoother fade-in
     }
 
-    private void PlayBreathing()
+    private static void PlayBreathing()
     {
-        if (!audioSource.isPlaying)
+        if (!_audioSource.isPlaying)
         {
             Log("Playing breathing sound");
-            audioSource.Play();
-        }
-        else
-        {
-            Log("Breathing sound is already playing");
+            _audioSource.Play();
         }
     }
 
     private void StopBreathing()
     {
-        if (audioSource.isPlaying)
-        {
-            Log("Stopping breathing sound");
-            audioSource.Stop();
-            PlayEndingSound();
-        }
+        if (!_audioSource.isPlaying) return;
+
+        //Log("Stopping breathing sound");
+        _audioSource.Stop();
+        PlayEndingSound();
     }
 
     private void PlayEndingSound()
     {
-        if (!endingAudioSource.isPlaying)
-        {
-            Log("Playing ending sound");
-            endingAudioSource.volume = VolumeReductionFactor; // Apply volume reduction
-            endingAudioSource.Play();
-        }
-        else
-        {
-            Log("Ending sound is already playing");
-        }
+        if (_endingAudioSource.isPlaying) return;
+        
+        _endingAudioSource.volume = VolumeReductionFactor;
+        _endingAudioSource.Play();
     }
 }
